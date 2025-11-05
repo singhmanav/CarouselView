2469142ce06fb16ded1aa02a7af5de53c2c2a580
Manav <manav.prakash@1mg.com>
2025-11-05 19:07:15 +0530

    Add app entry point
    - Create main app structure with SwiftUI App protocol
    - Configure ContentView as root view
    - Setup WindowGroup for app lifecycle

---
+++ atlys/atlysApp.swift
@@ -0,0 +1,17 @@
+//
+//  atlysApp.swift
+//  atlys
+//
+//  Created by Manav on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct atlysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
